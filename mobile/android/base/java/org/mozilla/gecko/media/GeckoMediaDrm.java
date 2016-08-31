a874baaf70b5f5cf7db1a66aae435c2c2bc056c5
James Cheng <jacheng@mozilla.com>
2016-08-31 16:00:31 +0800

    Part1 - Add MediaDrmBrige-related aidl and interface(GeckoMediaDrm.java),renamed ICodecManager to IMediaManage, CodecManager to MediaManager.
    MozReview-Commit-ID: FMFSoKJLoeW

---
+++ mobile/android/base/java/org/mozilla/gecko/media/GeckoMediaDrm.java
@@ -0,0 +1,30 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.gecko.media;
+
+public interface GeckoMediaDrm {
+    public interface Callbacks {
+        void onSessionCreated(int createSessionToken,
+                              int promiseId,
+                              byte[] sessionId,
+                              byte[] request);
+        void onSessionUpdated(int promiseId, byte[] sessionId);
+        void onSessionClosed(int promiseId, byte[] sessionId);
+        void onSessionMessage(byte[] sessionId,
+                              int sessionMessageType,
+                              byte[] request);
+        void onSessionError(int promiseId, byte[] sessionId);
+        void onSessionKeyChanged(byte[] sessionId, byte[] keyId, int statusCode);
+        void onError(String message);
+    }
+    void setCallbacks(Callbacks callbacks);
+    void createSession(int createSessionToken,
+                       int promiseId,
+                       String initDataType,
+                       byte[] initData);
+    void updateSession(int promiseId, String sessionId, byte[] response);
+    void closeSession(int promiseId, String sessionId);
+    void release();
+}
