06266a53cd31089f7112f41a08bfa6050c59a63c
Kilik Kuo <kikuo@mozilla.com>
2017-03-08 16:31:15 +0800

    Add several information APIs in GeckoHlsPlayer.

--- mobile/android/base/java/org/mozilla/gecko/media/GeckoHlsVideoRenderer.java
+++ mobile/android/base/java/org/mozilla/gecko/media/GeckoHlsVideoRenderer.java
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 package org.mozilla.gecko.media;
 
 import android.annotation.TargetApi;
@@ -966,5 +970,4 @@ public class GeckoHlsVideoRenderer extends BaseRenderer {
         }
 
     }
-
 }
